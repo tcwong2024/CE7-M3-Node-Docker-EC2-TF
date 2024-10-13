d5d9e3bad189b0b28e5883f8a5ce3b4248238eb4
tcwong2024 <tcwong99@hotmail.com>
2024-10-13 17:10:51 +0800

    Update compute add ingress TCP 8080

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -62,6 +62,13 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
